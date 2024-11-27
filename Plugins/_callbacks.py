3d0b1f5ca1d20cdfa2e172af3229e5be5cf6d885
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-28 00:56:47 +0530

    Update _callbacks.py

--- Plugins/_callbacks.py
+++ Plugins/_callbacks.py
@@ -5,6 +5,14 @@ from .settings import markup
 from Database.settings import get_settings, update_settings
 from .paid import pay_cbq
 
+async def toggle_setting(setting: str, current_value: bool) -> bool:
+    """Toggle a given setting value (True/False)."""
+    new_value = not current_value
+    dic = await get_settings()
+    dic[setting] = new_value
+    await update_settings(dic)
+    return new_value
+
 @Client.on_callback_query()
 async def cbq(_, q: CallbackQuery):
     data = q.data
@@ -18,59 +26,51 @@ async def cbq(_, q: CallbackQuery):
     elif data == 'connect':
         await q.answer()
         return await q.message.reply('Type /connect.')
+    
     if not q.from_user.id in SUDO_USERS:
         return await q.answer()
+
     if data == 'answer':
         await q.answer()
+    
+    # Settings toggles (Refactored to use toggle_setting function)
     elif data == 'toggle_approval':
-        dic = await get_settings()
-        dic['auto_approval'] = not dic['auto_approval']
-        mark = markup(dic)
+        new_value = await toggle_setting('auto_approval', (await get_settings())['auto_approval'])
         await q.answer()
-        await update_settings(dic)
-        await q.edit_message_reply_markup(reply_markup=mark)
+        await q.edit_message_reply_markup(reply_markup=markup(await get_settings()))
+    
     elif data == 'toggle_join':
-        dic = await get_settings()
-        dic['join'] = not dic['join']
-        mark = markup(dic)
+        new_value = await toggle_setting('join', (await get_settings())['join'])
         await q.answer()
-        await update_settings(dic)
-        await q.edit_message_reply_markup(reply_markup=mark)
+        await q.edit_message_reply_markup(reply_markup=markup(await get_settings()))
+    
     elif data == 'toggle_leave':
-        dic = await get_settings()
-        dic['leave'] = not dic['leave']
-        mark = markup(dic)
+        new_value = await toggle_setting('leave', (await get_settings())['leave'])
         await q.answer()
-        await update_settings(dic)
-        await q.edit_message_reply_markup(reply_markup=mark)
+        await q.edit_message_reply_markup(reply_markup=markup(await get_settings()))
+    
     elif data == 'toggle_image':
-        dic = await get_settings()
-        dic['image'] = not dic['image']
-        mark = markup(dic)
+        new_value = await toggle_setting('image', (await get_settings())['image'])
         await q.answer()
-        await update_settings(dic)
-        await q.edit_message_reply_markup(reply_markup=mark)
+        await q.edit_message_reply_markup(reply_markup=markup(await get_settings()))
+    
     elif data == 'toggle_gen':
         dic = await get_settings()
         dic['generate'] = 10 if dic.get('generate', 10) == 1 else 1
-        mark = markup(dic)
-        await q.answer()
         await update_settings(dic)
-        await q.edit_message_reply_markup(reply_markup=mark)
+        await q.answer()
+        await q.edit_message_reply_markup(reply_markup=markup(dic))
+    
     elif data == "toggle_save":
-        dic = await get_settings()
-        dic['auto_save'] = not dic.get('auto_save', False)
-        mark = markup(dic)
+        new_value = await toggle_setting('auto_save', (await get_settings()).get('auto_save', False))
         await q.answer()
-        await update_settings(dic)
-        await q.edit_message_reply_markup(reply_markup=mark)
+        await q.edit_message_reply_markup(reply_markup=markup(await get_settings()))
+    
     elif data == "toggle_logs":
-        dic = await get_settings()
-        dic['logs'] = not dic.get('logs', True)
-        mark = markup(dic)
+        new_value = await toggle_setting('logs', (await get_settings()).get('logs', True))
         await q.answer()
-        await update_settings(dic)
-        await q.edit_message_reply_markup(reply_markup=mark)
+        await q.edit_message_reply_markup(reply_markup=markup(await get_settings()))
+
+    # For activating features like ab, su, mc, ad, or activating paid users
     elif data.startswith(("toggleab", "togglesu", "togglemc", "togglead", "activate")):
         await pay_cbq(_, q)
-        
