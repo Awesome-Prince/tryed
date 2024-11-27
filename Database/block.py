70e1595683e8fd2564cc1b671d92f971ec1a8e82
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-28 00:46:19 +0530

    Update block.py

--- Database/block.py
+++ Database/block.py
@@ -1,16 +1,18 @@
 from . import db
 from config import BOT_TOKEN
 
+# Dynamically select the collection based on the bot token (first part before ':')
 db = db[f"block_{BOT_TOKEN.split(':')[0]}"]
 
+# Block a user by inserting their user_id into the database
 async def block(user_id):
     await db.insert_one({'user_id': user_id})
 
+# Unblock a user by deleting their user_id from the database
 async def unblock(user_id):
     await db.delete_one({'user_id': user_id})
 
+# Check if a user is blocked by querying the database for their user_id
 async def is_blocked(user_id):
     x = await db.find_one({'user_id': user_id})
-    if x:
-        return True
-    return False
+    return bool(x)  # Return True if user is blocked, otherwise False
