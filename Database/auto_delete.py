01ad4ed3f75c87f49e723b5cf52bae48d335bf83
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-28 00:43:49 +0530

    Update auto_delete.py

--- Database/auto_delete.py
+++ Database/auto_delete.py
@@ -1,17 +1,20 @@
 from . import db
 
-db = db.auto_delete
+db = db.auto_delete  # Access the 'auto_delete' collection from the 'db' module
 
+# Function to update or insert a document for a given user_id
 async def update(user_id, dic):
     await db.update_one({'user_id': user_id}, {'$set': {'dic': dic}}, upsert=True)
 
+# Function to retrieve the 'dic' field of a user document by user_id
 async def get(user_id):
     x = await db.find_one({'user_id': user_id})
     if x:
         return x['dic']
-    return {}
+    return {}  # Return an empty dictionary if no document is found
 
+# Function to fetch all user_ids from the collection and return them as a list
 async def get_all() -> list[int]:
-    x = db.find()
-    x = await x.to_list(length=None)
-    return [y['user_id'] for y in x]
+    x = db.find()  # Query to get all documents in the collection
+    x = await x.to_list(length=None)  # Convert the cursor to a list of documents
+    return [y['user_id'] for y in x]  # Extract and return the user_ids as a list
