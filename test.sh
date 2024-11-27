bc12beb616632ca7cf0ca63cf69c155fdf7f1864
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-28 00:42:27 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,20 @@
-
+# Pull the latest changes from the Git repository
 git pull
+
+# Clear the terminal to keep things clean
 clear
 
+# Check if the "manik" directory (for the virtual environment) doesn't exist
+# If it doesn't exist, create a new virtual environment
 if [ ! -d "manik" ]; then
     python3 -m venv manik
 fi
 
+# Activate the virtual environment
 source manik/bin/activate
+
+# Clear the terminal again after activating the virtual environment
 clear
+
+# Start the Python application by running start.py
 python3 start.py
