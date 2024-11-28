e878bf44401cea6aa4b74601b18916e94f894647
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-29 00:59:48 +0530

    Update auto_delete.py

--- Database/auto_delete.py
+++ Database/auto_delete.py
@@ -1,26 +1,21 @@
 from . import db
 
+# Accessing the auto_delete collection
 db = db.auto_delete
 
-async def update(user_id: int, dic: dict) -> None:
-    """
-    Update or insert the dictionary for a specific user.
-    """
+async def update(user_id, dic):
+    """Update or insert user data based on user_id."""
     await db.update_one({'user_id': user_id}, {'$set': {'dic': dic}}, upsert=True)
 
-async def get(user_id: int) -> dict:
-    """
-    Retrieve the dictionary for a specific user.
-    """
+async def get(user_id):
+    """Get user data based on user_id."""
     user_data = await db.find_one({'user_id': user_id})
     if user_data:
-        return user_data['dic']
+        return user_data.get('dic', {})
     return {}
 
 async def get_all() -> list[int]:
-    """
-    Retrieve a list of all user IDs in the auto_delete collection.
-    """
-    cursor = db.find()
-    user_list = await cursor.to_list(length=None)
-    return [user['user_id'] for user in user_list]
+    """Get all user_ids from the collection."""
+    all_users = db.find()
+    all_users_list = await all_users.to_list(length=None)
+    return [user['user_id'] for user in all_users_list]
