48c2123b8139678db773bf8cda5a7e76d6b051bf
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-29 00:51:51 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,20 +1,16 @@
 from main import start, app
 import os
 
-def purge_session_files():
-    """
-    Function to purge session files.
-    """
+# Function for purging session files
+def purge_sessions():
     print("Purging session files...")
     for filename in os.listdir():
         if filename.endswith(".session") or filename.endswith(".session-journal"):
             os.remove(filename)
 
-# Purge old session files
-purge_session_files()
+# Purge session files
+purge_sessions()
 
 print("Starting Bots...")
 os.system('clear')
-
-# Run the app with the start function
 app.run(start())
