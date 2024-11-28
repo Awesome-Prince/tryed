7fe49847e31a0fa615a5617dac5e952419cca85e
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-29 01:50:02 +0530

    Update encode_decode.py

--- Plugins/encode_decode.py
+++ Plugins/encode_decode.py
@@ -1,36 +1,39 @@
 import base64
 
-# Mapping for integer to character conversion
 a2j = 'abcdefghij'
 
 def Int2Char(x: int) -> str:
     """
-    Convert an integer to a string using the a2j mapping.
+    Converts an integer to a string using a custom alphabet.
     """
-    x_str = str(x)
-    int_list = [int(char) for char in x_str]
-    char_list = [a2j[digit] for digit in int_list]
+    int_str = str(x)
+    int_list = [int(char) for char in int_str]
+    char_list = [a2j[num] for num in int_list]
     return ''.join(char_list)
 
 def Char2Int(x: str) -> int:
     """
-    Convert a string back to an integer using the a2j mapping.
+    Converts a string using a custom alphabet back to an integer.
     """
     int_list = [a2j.index(char) for char in x]
-    int_str = ''.join([str(digit) for digit in int_list])
+    int_str = ''.join([str(num) for num in int_list])
     return int(int_str)
 
 def encrypt(txt: str) -> str:
     """
-    Encrypt a string using base64 encoding.
+    Encrypts a string using base64 encoding.
     """
-    return base64.b64encode(txt.encode('utf-8')).decode('utf-8').rstrip('=')
+    encoded_bytes = base64.b64encode(txt.encode('utf-8'))
+    encoded_str = encoded_bytes.decode('utf-8').rstrip('=')
+    return encoded_str
 
 def decrypt(txt: str) -> str:
     """
-    Decrypt a base64 encoded string.
+    Decrypts a base64 encoded string.
     """
-    padding = len(txt) % 4
-    if padding != 0:
-        txt += '=' * padding
-    return base64.b64decode(txt.encode('utf-8')).decode('utf-8')
+    padding_needed = len(txt) % 4
+    if padding_needed != 0:
+        txt += '=' * padding_needed
+    decoded_bytes = base64.b64decode(txt.encode('utf-8'))
+    decoded_str = decoded_bytes.decode('utf-8')
+    return decoded_str
