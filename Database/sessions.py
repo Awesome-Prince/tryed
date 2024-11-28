a706950eb9d9f0619e63f4303311f946f9ba4f4a
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-29 01:09:17 +0530

    Update sessions.py

--- Database/sessions.py
+++ Database/sessions.py
@@ -1,23 +1,17 @@
 from . import db
 
-# Select the collection for managing user sessions
+# Accessing the sessions collection
 db = db.sessions
 
-async def update_session(user_id: int, session: str) -> None:
-    """
-    Update or insert the session for a specific user.
-    """
+async def update_session(user_id, session):
+    """Update or insert a session for the given user_id."""
     await db.update_one({'user_id': user_id}, {'$set': {'session': session}}, upsert=True)
 
-async def get_session(user_id: int) -> str:
-    """
-    Retrieve the session for a specific user.
-    """
+async def get_session(user_id):
+    """Get the session for the given user_id."""
     user_data = await db.find_one({'user_id': user_id}) or {}
     return user_data.get('session', None)
 
-async def del_session(user_id: int) -> None:
-    """
-    Delete the session for a specific user.
-    """
+async def del_session(user_id):
+    """Delete the session for the given user_id."""
     await db.delete_one({'user_id': user_id})
