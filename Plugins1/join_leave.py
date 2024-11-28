243e3590d7bb4559b7a03931b076ca2592f3c0ca
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-28 17:32:29 +0530

    Update join_leave.py

--- Plugins1/join_leave.py
+++ Plugins1/join_leave.py
@@ -1,35 +1,29 @@
 from pyrogram import Client, filters
-from pyrogram.types import ChatMemberUpdated
-from config import (
-    FSUB_1, FSUB_2, TUTORIAL_LINK,
-)
+from pyrogram.types import ChatMemberUpdated, InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
+from config import FSUB_1, FSUB_2, TUTORIAL_LINK
 from templates import LEAVE_MESSAGE
 from Database.settings import get_settings
 from Plugins.start import get_chats
-from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
 
+# List of subscription channels
 FSUB = [FSUB_1, FSUB_2]
 
 markup = None
 
-async def build(_):
-    """Builds the inline keyboard markup with dynamic invite links for the chat."""
+async def build(client: Client):
+    """
+    Build the markup with invite links for the subscription channels.
+    """
     global markup
     if not markup:
-        chats = await get_chats(_)  # Fetch chat details
-        new = []
+        chats = await get_chats(client)
+        new_links = []
         for chat in chats:
-            # Generate invite links for the chats
-            invite_link = await _.create_chat_invite_link(chat.id, creates_join_request=True)
-            new.append(invite_link.invite_link)
-        
-        # Assign generated invite links to the chats
-        for i, link in enumerate(new):
-            chats[i].invite_link = link
-        
-        # Get the first two chats and set up the inline keyboard markup
+            invite_link = await client.create_chat_invite_link(chat.id, creates_join_request=True)
+            new_links.append(invite_link.invite_link)
+        for i, invite_link in enumerate(new_links):
+            chats[i].invite_link = invite_link
         chat = chats[0]
-        chat1 = chats[1]  # This seems unused, but could be for a future feature
         markup = IKM(
             [
                 [
@@ -44,52 +38,38 @@ async def build(_):
     return markup
 
 @Client.on_chat_member_updated(filters.chat(FSUB_1))
-async def jl(_: Client, cmu: ChatMemberUpdated):
-    """Handles user join/leave events in the specified channels."""
-    
-    user = cmu.from_user
-    left = cmu.old_chat_member and not cmu.new_chat_member  # Check if the user left
-    joined = cmu.new_chat_member and not cmu.old_chat_member  # Check if the user joined
-    
-    # If the user neither joined nor left, we don't need to proceed
+async def jl(client: Client, chat_member_update: ChatMemberUpdated):
+    """
+    Handle chat member updates (join/leave events).
+    """
+    user = chat_member_update.from_user
+    left = chat_member_update.old_chat_member and not chat_member_update.new_chat_member
+    joined = chat_member_update.new_chat_member and not chat_member_update.old_chat_member
     if not left and not joined:
         return
-    
     settings = await get_settings()
-    markup = await build(_)  # Get the inline keyboard markup
-    
+    markup = await build(client)
     if joined:
-        # Handle the case when the user joins
+        """
         if not settings['join']:
-            return  # If joining is disabled, do nothing
+            return
         try:
-            # Send a welcome message or image when the user joins
-            if JOIN_IMAGE:  # Check if there is a join image
-                await _.send_photo(
-                    user.username if user.username else user.id,
-                    JOIN_IMAGE,
-                    caption=JOIN_MESSAGE.format(user.mention),  # Format the join message with user mention
-                    reply_markup=markup
-                )
+            if JOIN_IMAGE:
+                await client.send_photo(user.username if user.username else user.id, JOIN_IMAGE, caption=JOIN_MESSAGE, reply_markup=markup)
             else:
-                await _.send_message(
-                    user.username if user.username else user.id,
-                    JOIN_MESSAGE.format(user.mention),
-                    reply_markup=markup
-                )
+                await client.send_message(user.username if user.username else user.id, JOIN_MESSAGE, reply_markup=markup)
         except Exception as e:
-            print(f"Error sending join message: {e}")
+            print(e)
+        """
+        ...
     else:
-        # Handle the case when the user leaves
         if not settings['leave']:
-            return  # If leaving is disabled, do nothing
+            return
         try:
-            # Send a leave voice message (if applicable)
-            await _.send_voice(
-                user.username if user.username else user.id,
-                'Voice/uff.ogg',  # Path to the leave voice message
-                caption=LEAVE_MESSAGE.format(user.mention),  # Format the leave message with user mention
-                reply_markup=markup
-            )
+            # if LEAVE_IMAGE:
+            #     await client.send_photo(user.username if user.username else user.id, LEAVE_IMAGE, caption=LEAVE_MESSAGE, reply_markup=markup)
+            # else:
+            #     await client.send_message(user.username if user.username else user.id, LEAVE_MESSAGE, reply_markup=markup)
+            await client.send_voice(user.username if user.username else user.id, 'Voice/uff.ogg', caption=LEAVE_MESSAGE, reply_markup=markup)
         except Exception as e:
-            print(f"Error sending leave message: {e}")
+            print(e)
