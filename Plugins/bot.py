edb38b0ea3ee09b280cc5a7a9fa7615c4242b3be
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-28 16:58:42 +0530

    Update bot.py

--- Plugins/bot.py
+++ Plugins/bot.py
@@ -1,119 +1,175 @@
+import asyncio
+import os
+import time
 from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from pyrogram.handlers import MessageHandler
 from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
-from Database.sessions import *
-from Database.privileges import get_privileges
-from config import API_ID, API_HASH, USELESS_IMAGE, PHONE_NUMBER_IMAGE
-from pyrogram.errors import (
-    SessionPasswordNeeded,
-    PhoneNumberInvalid,
-    PhoneCodeInvalid,
-    PasswordHashInvalid
+
+from config import (
+    API_HASH,
+    API_ID,
+    AUTO_DELETE_TIME,
+    AUTO_SAVE_CHANNEL_ID,
+    WARN_IMAGE,
+    USELESS_IMAGE,
+    USELESS_MESSAGE
 )
-from . import build, tryer
-from templates import USELESS_MESSAGE
+from Database.auto_delete_2 import update_2, get_all_2, get_2
+from Database.count_2 import incr_count_2
+from Database.privileges import get_privileges
+from Database.sessions import get_session
+from Database.settings import get_settings
+from main import app as paa, ClientLike
+from templates import AUTO_DELETE_TEXT, POST_DELETE_TEXT
+from . import AUTO_DELETE_STR, tryer, build
 
-# Inline keyboard markup for the phone number shortcut button
-phone_markup = IKM([[IKB('𝘚𝘩𝘰𝘳𝘵𝘤𝘶𝘵', url='tg://settings')]])
+# Global variables
+global_app = {}
+me = None
+bots = {}
 
-# Dictionary to track ongoing connection processes
-ongoing_connections = {}
-watch_group = 69
+# Template for warning message
+TEMPL = '''┏━━━━━𓆩𝘌𝘳𝘳𝘰𝘳 𝘍𝘰𝘶𝘯𝘥𓆪ꪾ━━━━━┓
+♛ 𝙃𝙚𝙮 𝙏𝙂 𝙐𝙨𝙚𝙧 : {}
 
-def is_in_work(user_id):
-    return user_id in ongoing_connections
+≼𝗪𝗔𝗥𝗡𝗜𝗡𝗚 𝗔𝗟𝗘𝗥𝗧≽
+➤**You are Not eligible To Save BOT 
+Content ....
 
-@Client.on_message(filters.command('connect') & filters.private)
-async def connect_handler(client, message):
-    user_id = message.from_user.id
-    privileges = await get_privileges(user_id)
-    if not privileges[1]:
+➤If You Try To Save Again, Your 
+Membership Will Be Dead...**
+┗━━━━━━𓆩𝘛𝘦𝘳𝘢𝘉𝘰𝘹𓆪ꪾ━━━━━━┛'''
+
+# Inline keyboard markup
+markup = IKM([[IKB('𝘚𝘩𝘢𝘳𝘦 𝘞𝘪𝘵𝘩 𝘔𝘦', callback_data='sharewithme')]])
+
+async def stop(client):
+    try:
+        await client.stop()
+    except ConnectionError:
+        pass
+
+# Function to handle saving messages
+async def save(C, M):
+    global me
+
+    if not M.chat.id in bots:
+        bots[M.chat.id] = (await tryer(C.get_users, M.chat.id)).is_bot
+
+    if not me:
+        me = await paa.get_me()
+
+    priv = await get_privileges(M.from_user.id)
+    if not priv[2]:
+        if M.chat.id == me.id:
+            return await paa.send_photo(M.from_user.id, WARN_IMAGE, caption=TEMPL.format(M.from_user.mention))
+    
+    dm = not bots[M.chat.id]
+    if not priv[3]:
+        if dm:
+            return await tryer(M.delete)
+    
+    try:
+        count = int(M.text.split()[1])
+    except:
+        count = 1
+
+    if count > 20:
+        return await M.edit('Limit is 20.')
+
+    if not M.reply_to_message:
+        return await M.edit('Reply to a file to save.')
+
+    settings = await get_settings()
+    st = M.reply_to_message.id
+    en = st + count
+    messes = await C.get_messages(M.chat.id, list(range(st, en)))
+    count = await incr_count_2()
+    cops = []
+
+    uffie = await tryer(paa.send_message, M.from_user.id, 'Under processing...')
+
+    for msg in messes:
+        if not msg or msg.empty:
+            continue 
+        if msg.from_user.id == M.from_user.id:
+            continue
+        if msg.text:
+            await uffie.delete()
+            cop = await paa.send_message(M.from_user.id, msg.text, reply_markup=markup)
+        else:
+            if dm:
+                if not msg.caption:
+                    msg.caption = '#DM'
+                else:
+                    msg.caption = '#DM\n ' + msg.caption
+            try:
+                dl = await msg.download()
+                await uffie.delete()
+                if msg.document:
+                    cop = await paa.send_document(M.from_user.id, dl, caption=msg.caption, reply_markup=markup)
+                elif msg.video:
+                    cop = await paa.send_video(M.from_user.id, dl, caption=msg.caption, reply_markup=markup)
+                elif msg.photo:
+                    cop = await paa.send_photo(M.from_user.id, dl, caption=msg.caption, reply_markup=markup)
+                elif msg.animation:
+                    cop = await paa.send_animation(M.from_user.id, dl, caption=msg.caption, reply_markup=markup)
+                os.remove('downloads/' + dl.split('/')[-1])
+            except:
+                pass
+        if settings['auto_save']:
+            await cop.copy(AUTO_SAVE_CHANNEL_ID, reply_markup=None)
+        cops.append(cop.id)
+
+    ok = await paa.send_message(M.from_user.id, AUTO_DELETE_TEXT.format(AUTO_DELETE_STR))
+    await update_2(M.from_user.id, [cops, ok.id, count, time.time()])
+    
+    # await stop(global_app[M.from_user.id])
+
+@Client.on_message(filters.command('bot'))
+async def bot(client: Client, message: Message):
+    id = message.from_user.id
+    priv = await get_privileges(id)
+    if not priv[1]:
         return await tryer(message.reply_photo, USELESS_IMAGE, caption=USELESS_MESSAGE, reply_markup=await build(client))
-    
-    session = await get_session(user_id)
-    if session:
-        app = Client(str(user_id), api_id=API_ID, api_hash=API_HASH, session_string=session)
+
+    session = await get_session(id)
+    if not session:
+        return await message.reply("**Before Use.You Have to Connect with Bot. For Connect Use: /connect **")
+
+    try:
+        app = ClientLike(str(id), api_id=API_ID, api_hash=API_HASH, session_string=session)
+        await app.start()
+        global_app[id] = app
+        await message.reply('**UBot Activated\nUse  `..`  To Save Other Bot Content Or User DM Content.**')
+        app.add_handler(MessageHandler(save, (filters.command('.', '.') & filters.me)))
+        await asyncio.sleep(300)
         try:
-            await app.start()
-            await message.reply("**You Are Already Connected User**")
             await app.stop()
-            return
-        except:
-            await del_session(user_id)
-    
-    if is_in_work(user_id):
-        return await message.reply("**Process Ongoing..., use /terminate to cancel.**")
-    
-    await message.reply_photo(PHONE_NUMBER_IMAGE, caption=(
-        "**Enter Your Phone Number With Country Code.\n"
-        "<pre>How To Find Number?</pre>\n"
-        "You can Use Shortcut Button To Find Your Number\n"
-        "<pre>Incase Shortcut Button Not Working Then You Need To Find Manually</pre>"
-    ), reply_markup=phone_markup)
-    
-    client_instance = Client(str(user_id), api_id=API_ID, api_hash=API_HASH)
-    ongoing_connections[user_id] = [client_instance]
-    await client_instance.connect()
-
-@Client.on_message(filters.private, group=watch_group)
-async def continue_workflow_handler(client, message):
-    user_id = message.from_user.id
-    if not is_in_work(user_id):
-        return
-    if not message.text or message.text.startswith("/"):
-        return
-
-    workflow_data = ongoing_connections[user_id]
-    if len(workflow_data) == 1:
-        client_instance = workflow_data[0]
-        workflow_data.append(message.text)
-        try:
-            code_hash = await client_instance.send_code(message.text)
+            await tryer(message.reply, '**UBot Deactivate..**')
         except ConnectionError:
-            await client_instance.connect()
-            code_hash = await client_instance.send_code(message.text)
-        except PhoneNumberInvalid:
-            ongoing_connections.pop(user_id)
-            return await message.reply('**Phone Number Is Invalid.**', reply_markup=phone_markup)
-        workflow_data.append(code_hash.phone_code_hash)
-        await message.reply("**Enter OTP:**")
-    elif len(workflow_data) == 3:
-        client_instance = workflow_data[0]
-        otp_code = message.text.replace(" ", "")
-        workflow_data.append(otp_code)
-        try:
-            await client_instance.sign_in(workflow_data[1], workflow_data[2], workflow_data[3])
-            session_string = await client_instance.export_session_string()
-            await update_session(user_id, session_string)
-            await client_instance.disconnect()
-            ongoing_connections.pop(user_id)
-            await message.reply("**Successfully Connected..**")
-        except PhoneCodeInvalid:
-            ongoing_connections.pop(user_id)
-            return await message.reply('**Invalid OTP!**')
-        except SessionPasswordNeeded:
-            await message.reply("**Enter Two Step Verification Password:**")
-    elif len(workflow_data) == 4:
-        client_instance = workflow_data[0]
-        workflow_data.append(message.text)
-        try:
-            await client_instance.check_password(workflow_data[4])
-            await client_instance.sign_in(workflow_data[1], workflow_data[2], workflow_data[3])
-            session_string = await client_instance.export_session_string()
-            await update_session(user_id, session_string)
-            await client_instance.disconnect()
-            ongoing_connections.pop(user_id)
-            await message.reply("**Connected Successfully.**")
-        except PhoneCodeInvalid:
-            ongoing_connections.pop(user_id)
-            return await message.reply('**Invalid OTP!**')
-        except PasswordHashInvalid:
-            ongoing_connections.pop(user_id)
-            return await message.reply('**Invalid Two Step Verification Password.**')
+            pass
+    except FloodWait as e:
+        return await message.reply(f'Try Again After {e.value} seconds.')
+    except:
+        return await message.reply('Session Expired.')
 
-@Client.on_message(filters.command("terminate"))
-async def terminate_handler(client, message):
-    user_id = message.from_user.id
-    if not is_in_work(user_id):
-        return
-    ongoing_connections.pop(user_id)
-    await message.reply("**Process Terminated.**")
+async def task():
+    while True:
+        x = await get_all_2()
+        for y in x:
+            lis = await get_2(y)
+            if not lis:
+                continue
+            if int(time.time() - lis[3]) < AUTO_DELETE_TIME:
+                continue
+            await tryer(paa.delete_messages, y, lis[0])
+            try:
+                await tryer(paa.edit_message_text, y, lis[1], POST_DELETE_TEXT.format(lis[2]))
+            except:
+                pass
+            await update_2(y, [])
+        await asyncio.sleep(10)
+
+asyncio.create_task(task())
