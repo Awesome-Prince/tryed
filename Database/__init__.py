999e85088da0a449ce8bb761e2bf3c87f03ae3e0
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-28 16:30:06 +0530

    Update __init__.py

--- Database/__init__.py
+++ Database/__init__.py
@@ -1,8 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_DB_URI
 
-# Create an AsyncIOMotorClient instance with the MongoDB URI from the config
+# Initialize the MongoDB client
 mongo = AsyncIOMotorClient(MONGO_DB_URI)
 
-# Access the 'SpL' database from the MongoDB client
+# Select the database
 db = mongo.SpL
