26f84c1d498d4d086f5add24ed69d8d603b9086b
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-29 02:24:45 +0530

    Update watchers.py

--- Plugins/watchers.py
+++ Plugins/watchers.py
@@ -1,12 +1,9 @@
 from pyrogram import Client, filters
-from pyrogram.types import (
-    Message, InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
-)
+from pyrogram.types import Message, InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
 from config import (
     SUDO_USERS, DB_CHANNEL_ID, DB_CHANNEL_2_ID,
     LOG_CHANNEL_ID, LINK_GENERATE_IMAGE,
-    USELESS_IMAGE,
-    TUTORIAL_LINK
+    USELESS_IMAGE, TUTORIAL_LINK
 )
 from templates import USELESS_MESSAGE, LINK_GEN
 from .encode_decode import encrypt, Int2Char
@@ -16,79 +13,84 @@ from .batch import in_batch, batch_cwf as bcwf
 from .block import block_dec
 from . import alpha_grt, tryer
 from pyrogram.errors import FloodWait
+import threading
 import asyncio
 from .connect import in_work
 from .get import get
 from . import build
 
 watch = 1
-
 me = None
 
-async def get_me(client: Client):
+async def get_me(_):
     """
-    Get the bot's own user information.
+    Get the bot's user information if not already fetched.
     """
     global me
     if not me:
-        me = await client.get_me()
+        me = await _.get_me()
     return me
 
 @Client.on_message(filters.private, group=watch)
 @block_dec
-async def cwf(client: Client, message: Message):
+async def cwf(_: Client, m: Message):
     """
-    Handle incoming private messages and perform necessary actions.
+    Handles incoming private messages.
     """
-    if in_work(message.from_user.id):
+    if in_work(m.from_user.id):
         return
-    if in_batch(message.from_user.id):
-        return await bcwf(client, message)
-    if message.text and message.text.startswith("https://t.me/"):
-        ret = await get(client, message)
+    if in_batch(m.from_user.id):
+        return await bcwf(_, m)
+    if m.text and m.text.startswith("https://t.me/"):
+        ret = await get(_, m)
         if ret:
             return
-    if message.from_user.id not in SUDO_USERS:
-        if message.text:
-            if not message.text.lower().startswith(('/start', '/terminate', '/connect', '/bot', '..', '/batch', '/id')):
-                markup = await build(client)
+    if not m.from_user.id in SUDO_USERS:
+        if m.text:
+            if not m.text.lower().startswith(('/start', '/terminate', '/connect', '/bot', '..', '/batch', '/id')):
+                markup = await build(_)
                 if USELESS_IMAGE:
-                    await message.reply_photo(USELESS_IMAGE, caption=USELESS_MESSAGE, reply_markup=markup)
+                    await m.reply_photo(USELESS_IMAGE, caption=USELESS_MESSAGE, reply_markup=markup)
                 else:
-                    await message.reply(USELESS_MESSAGE, reply_markup=markup)
+                    await m.reply(USELESS_MESSAGE, reply_markup=markup)
         else:
-            markup = await build(client)
+            markup = await build(_)
             if USELESS_IMAGE:
-                await message.reply_photo(USELESS_IMAGE, caption=USELESS_MESSAGE, reply_markup=markup)
+                await m.reply_photo(USELESS_IMAGE, caption=USELESS_MESSAGE, reply_markup=markup)
             else:
-                await message.reply(USELESS_MESSAGE, reply_markup=markup)
+                await m.reply(USELESS_MESSAGE, reply_markup=markup)
         return
-    if message.text and message.text.startswith('/'):
+    if m.text and m.text.startswith('/'):
         return
-    
-    settings = await get_settings()
 
+    settings = await get_settings()
+    """
+    if LINK_GENERATE_IMAGE and settings['image']:
+        try:
+            msg = await m.reply_photo(LINK_GENERATE_IMAGE, caption='**Generating Link...**', quote=True)
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            msg = await m.reply_photo(LINK_GENERATE_IMAGE, caption='**Generating Link...**', quote=True)
+    else:
+        try:
+            msg = await m.reply('**Generating Link...**', quote=True)
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            msg = await m.reply('**Generating Link...**', quote=True)
+    """
     res = await asyncio.gather(
-        tryer(message.copy, DB_CHANNEL_ID),
-        tryer(message.copy, DB_CHANNEL_2_ID)
+        tryer(m.copy, DB_CHANNEL_ID),
+        tryer(m.copy, DB_CHANNEL_2_ID)
     )
-    
     count = await incr_count()
     encr = encrypt(f'{Int2Char(res[0].id)}|{Int2Char(count)}|{Int2Char(res[1].id)}')
-    link = f'https://t.me/{(await get_me(client)).username}?start=get{encr}'
-    
-    if message.video:
-        dur = "⋞⋮⋟ " + alpha_grt(message.video.duration)
-    else:
-        dur = ''
-    
-    txt = LINK_GEN.format(str(count), dur, link)
+    link = f'https://t.me/{(await get_me(_)).username}?start=get{encr}'
+    duration_text = f"⋞⋮⋟ {alpha_grt(m.video.duration)}" if m.video else ''
+    txt = LINK_GEN.format(str(count), duration_text, link)
     markup = IKM([[IKB('Share', url=link)]])
-    
     if LINK_GENERATE_IMAGE and settings['image']:
-        msg = await tryer(message.reply_photo, LINK_GENERATE_IMAGE, caption=txt, quote=True)
+        msg = await tryer(m.reply_photo, LINK_GENERATE_IMAGE, caption=txt, quote=True)
     else:
-        msg = await tryer(message.reply, txt, quote=True)
-    
+        msg = await tryer(m.reply, txt, quote=True)
     if LOG_CHANNEL_ID and settings.get('logs', True):
         await tryer(msg.copy, LOG_CHANNEL_ID)
