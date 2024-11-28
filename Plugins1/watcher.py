a97bdcea68f28a3d6f0e682c688ae708af7ed145
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-29 02:29:41 +0530

    Update watcher.py

--- Plugins1/watcher.py
+++ Plugins1/watcher.py
@@ -2,14 +2,21 @@ from pyrogram import Client, filters
 from config import SUDO_USERS
 from Database.users import add_user_2
 
+# Group ID for processing private messages
 watch = 69
 
 @Client.on_message(filters.private, group=watch)
-async def cwf(client: Client, message: Message):
+async def cwf(_, m):
     """
-    Handle incoming private messages and add the user to the database.
+    Handles incoming private messages. If the sender is a SUDO user, adds them to the database.
+    Otherwise, replies with a predefined message and adds the sender to the database.
     """
-    if message.from_user and message.from_user.id in SUDO_USERS:
-        return await add_user_2(message.from_user.id)
-    await message.reply("**Message Me Here @CuteGirlTG**")
-    await add_user_2(message.from_user.id)
+    # If the sender is a SUDO user, add them to the database
+    if m.from_user and m.from_user.id in SUDO_USERS:
+        return await add_user_2(m.from_user.id)
+    
+    # Reply with a predefined message for non-SUDO users
+    await m.reply("**Message Me Here @CuteGirlTG**")
+    
+    # Add the sender to the database
+    await add_user_2(m.from_user.id)
