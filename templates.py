6031954fa5715390fb68a12dfe4ecb3e4def73ee
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-29 00:55:08 +0530

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,42 +1,47 @@
-# Templates for bot messages
-
-# Link generation template
 LINK_GEN = """
 ┏━━━༺𝘍𝘈𝘕𝘉𝘢𝘴𝘦༻━━━┓
 ☞ 𝘥𝘦𝘴𝘤𝘳𝘪𝘱𝘵𝘪𝘰𝘯: @Ultra_XYZ
+
 ☞ 𝘦𝘱𝘪𝘴𝘰𝘥𝘦 : {} {}
 ┗━━━༺▾𝖡𝖮𝖳𝘓𝘪𝘯𝘬▾༻━━━┛
 
 {}
 """
 
-# Useless message template
-USELESS_MESSAGE = "𓆩𝗘𝗥𝗥𝗢𝗥 𝗙𝗢𝗨𝗡𝗗𓆪ꪾ\n<b>𝘗𝘭𝘦𝘢𝘴𝘦 𝘋𝘰𝘯'𝘵 𝘚𝘦𝘯𝘥 𝘜𝘯𝘸𝘢𝘯𝘵𝘦𝘥 𝘔𝘦𝘴𝘴𝘢𝘨𝘦𝘴</b>\n\n♛ 𝙏𝙝𝙖𝙣𝙠𝙔𝙤𝙪 ♛\n𝘾𝙝𝙚𝙘𝙠 𝙊𝙐𝙏 𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡 𝘾𝙝𝙖𝙣𝙣𝙚𝙡 𖡦"
+USELESS_MESSAGE = """𓆩𝗘𝗥𝗥𝗢𝗥 𝗙𝗢𝗨𝗡𝗗𓆪ꪾ
+<b>𝘗𝘭𝘦𝘢𝘴𝘦 𝘋𝘰𝘯'𝘵 𝘚𝘦𝘯𝘥 𝘜𝘯𝘸𝘢𝘯𝘵𝘦𝘥 𝘔𝘦𝘴𝘴𝘢𝘨𝘦𝘴</b>
 
-# Leave message template
-LEAVE_MESSAGE = "<pre>Hey, Why Are You Leaving? Please Don't Go! I am Useless Without You🥹😢</pre>\n\n\n<pre>If Any Problem, then Talk To Me. BUT don't leave Me 😭💔</pre>\n\n𝗠𝗬 𝗧𝗚 𝗜𝗗: @CuteGirlTG"
+♛ 𝙏𝙝𝙖𝙣𝙠𝙔𝙤𝙪 ♛
+𝘾𝙝𝙚𝙘𝙠 𝙊𝙐𝙏 𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡 𝘾𝙝𝙖𝙣𝙣𝙚𝙡 𖡦
+"""
 
-# Auto-delete message template
-AUTO_DELETE_TEXT = " **•This Will Be AUTO DELETED IN {}\n╼╾╼╾╼╾╼⊰☝🏻⊱╾╼╾╼╾╼╾\n ☛ WHY?~ Due To Copyrights.** "
+LEAVE_MESSAGE = """<pre>Hey Why Are You Leaving ME? Please Don't Go, I am Useless Without You🥹😢</pre>
 
-# Post delete text template
-POST_DELETE_TEXT = """ **EXTREMELY SORRY BUT!!!!!
+<pre>If Any Problem then Talk To Me 
+BUT don't leave Me 😭💔</pre>
+
+𝗠𝗬 𝗧𝗚 𝗜𝗗 : @CuteGirlTG
+"""
+
+AUTO_DELETE_TEXT = """**•This Will Be AUTO DELETED IN {}
+╼╾╼╾╼╾╼⊰☝🏻⊱╾╼╾╼╾╼╾
+ ☛ WHY?~  Due To CopyRights.**
+"""
+
+POST_DELETE_TEXT = """**EXTREMELY SORRY BUT!!!!!
 ╼╾╼╾╼╾╼⊰𝘦𝘱 {} ⊱╾╼╾╼╾╼╾
-❖MEDIA SUCCESSFULLY DELETED** """
+❖MEDIA SUCCESSFULLY DELETED**
+"""
 
-# Join message template
 JOIN_MESSAGE = """♛ 𝙃𝙚𝙮 𝙏𝙂 𝙐𝙨𝙚𝙧 : {} 
-<b>Your Joining Request IS Accepted
+<b>Your Joining Request IS Accepted 
 
-<pre>𝙄𝙢𝙥𝙤𝙧𝙩𝙖𝙣𝙩 𝙉𝙤𝙩𝙚:
-If you don't know how to open the link, then click on the tutorial given below 𖡶</pre>
+<pre>𝙄𝙢𝙥𝙤𝙧𝙩𝙖𝙣𝙩 𝙉𝙤𝙩𝙚 : 
+If you don't know how to open link then click on tutorial given Below 𖡶</pre>
 
-Welcome To Shanaya FANBase Channel</b>"""
+Welcome To Shanaya FANBase Channel</b>
+"""
 
-# Start message template 1
-START_MESSAGE = "┏━━━━𓆩𝘌𝘳𝘳𝘰𝘳 𝘍𝘰𝘶𝘯𝘥𓆪ꪾ━━━━┓\n ♛ 𝙃𝙚𝙮 𝙏𝙂 𝙐𝙨𝙚𝙧 : {} \n\n ≼𝙄𝙈𝙋𝙊𝙍𝙏𝘼𝙉𝙏≽ \n **➤ If You Want To Use BOT, You \nHave To JOIN Both Channels. \n\n ➤IF You Facing Any Problem You \nCan Click On Tutorial Button ⇊** \n ┗━━━━━𓆩𝘛𝘦𝘳𝘢𝘉𝘰𝘹𓆪ꪾ━━━━━┛"
-
-# Start message template 2
 START_MESSAGE_2 = """┏━━━━𓆩𝘌𝘳𝘳𝘰𝘳 𝘍𝘰𝘶𝘯𝘥𓆪ꪾ━━━━┓
 ♛ 𝙃𝙚𝙮 𝙏𝙂 𝙐𝙨𝙚𝙧 : {}
 
@@ -45,31 +50,52 @@ START_MESSAGE_2 = """┏━━━━𓆩𝘌𝘳𝘳𝘰𝘳 𝘍𝘰𝘶𝘯�
 
 ➤ Use CORRECT COMMAND With Help
  Of The Channel Given Below</b>⍗
-┗━━━━━𓆩𝘛𝘦𝘳𝘢𝘉𝘰𝘹𓆪ꪾ━━━━━┛"""
+┗━━━━━𓆩𝘛𝘦𝘳𝘢𝘉𝘰𝘹𓆪ꪾ━━━━━┛
+"""
 
-# Try again text template
-TRY_AGAIN_TEXT = "┏━━━━𓆩𝘌𝘳𝘳𝘰𝘳 𝘍𝘰𝘶𝘯𝘥𓆪ꪾ━━━━┓\n♛ 𝙃𝙚𝙮 𝙏𝙂 𝙐𝙨𝙚𝙧 : {} \n\n≼𝗪𝗔𝗥𝗡𝗜𝗡𝗚 𝗔𝗟𝗘𝗥𝗧≽\n➤ **Before Use. ME You Have To Must\nJOIN Both Channels\n\n➤ Please JOIN It And When You JOINED \nBoth Channels Then ♻️Try Again** \n┗━━━━━𓆩𝘛𝘦𝘳𝘢𝘉𝘰𝘹𓆪ꪾ━━━━━┛"
+START_MESSAGE = """┏━━━━𓆩𝘌𝘳𝘳𝘰𝘳 𝘍𝘰𝘶𝘯𝘥𓆪ꪾ━━━━┓
+♛ 𝙃𝙚𝙮 𝙏𝙂 𝙐𝙨𝙚𝙧 : {} 
 
-# Super user text template
-SU_TEXT = """🎉 𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘂𝗹𝗮𝘁𝗶𝗼𝗻𝘀 {}
+≼𝙄𝙈𝙋𝙊𝙍𝙏𝘼𝙉𝙏≽ 
+**➤ If You Want To Use BOT, You 
+Have To JOIN Both Channels. 
 
-≼𝙎𝙪𝙥𝙚𝙧 𝙐𝙨𝙚𝙧≽:
-**➤ Now You Can Store Data Easily. If You Don't Know How to Use It, You Can See The Tutorial Video**
+➤ If You Are Facing Any Problem You 
+Can Click On Tutorial Button ⇊** 
+┗━━━━━𓆩𝘛𝘦𝘳𝘢𝘉𝘰𝘹𓆪ꪾ━━━━━┛
+"""
 
-≼𝙄𝙈𝙋𝙊𝙍𝙏𝘼𝙉𝙏≽:
-**➤ If You Can See CONNECT Button,
-You Need To Set It Up Before Using Me**
+TRY_AGAIN_TEXT = """┏━━━━𓆩𝘌𝘳𝘳𝘰𝘳 𝘍𝘰𝘶𝘯𝘥𓆪ꪾ━━━━┓
+♛ 𝙃𝙚𝙮 𝙏𝙂 𝙐𝙨𝙚𝙧 : {} 
 
-<pre>𝙀𝙓𝙋𝙄𝙍𝙀 𝘼𝙏 ➤ {}</pre>"""
+≼𝗪𝗔𝗥𝗡𝗜𝗡𝗚 𝗔𝗟𝗘𝗥𝗧≽
+➤ **Before Use ME, You Have To Must
+JOIN Both Channels.
 
-# Expiry message template
+➤ Please JOIN It And When You JOINED 
+Both Channels Then ♻️ Try Again** 
+┗━━━━━𓆩𝘛𝘦𝘳𝘢𝘉𝘰𝘹𓆪ꪾ━━━━━┛
+"""
+
+SU_TEXT = """🎉 𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘂𝗹𝗮𝘁𝗶𝗼𝗻𝘀  {}
+
+≼𝙎𝙪𝙥𝙚𝙧 𝙐𝙨𝙚𝙧≽ :
+**➤  Now You Can Store Data Easily. If You Don't Know How to Use It, You Can See The Tutorial Video**
+
+≼𝙄𝙈𝙋𝙊𝙍𝙏𝘼𝙉𝙏≽ :
+**➤ If You Can See CONNECT Button 
+So You Want To Setup Before Use me**
+
+<pre>𝙀𝙓𝙋𝙄𝙍𝙀 𝘼𝙏 ➤ {}</pre>
+"""
+
 EXPIRE_TEXT = """
-♛ 𝙃𝙚𝙮 𝙏𝙂 𝙐𝙨𝙚𝙧 :{}
+♛ 𝙃𝙚𝙮 𝙏𝙂 𝙐𝙨𝙚𝙧 : {}
 
-≼𝙈𝙚𝙢𝙗𝙚𝙧𝙨𝙝𝙞𝙥 𝙀𝙭𝙥𝙞𝙧𝙚𝙙≽
+≼𝙈𝙚𝙢𝙚𝙗𝙚𝙧𝙨𝙝𝙞𝙥 𝙀𝙭𝙥𝙞𝙧𝙚𝙙≽
 **➤ Sorry To Say {} But Your Plan 
-Is Expired Now. But You Can RENEW**
+Has Expired Now. You Can RENEW**
 
-≼𝙁𝙊𝙍 𝙍𝙀𝙉𝙀𝙒≽:
+≼𝙁𝙊𝙍 𝙍𝙀𝙉𝙀𝙒≽ :
 ➤ If You Want To RENEW Then Click On The Button Below.
 """
