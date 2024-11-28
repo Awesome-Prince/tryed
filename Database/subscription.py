30a291038d76de40f26f086ce48979ed2d4ca198
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-29 01:11:13 +0530

    Update subscription.py

--- Database/subscription.py
+++ Database/subscription.py
@@ -1,25 +1,19 @@
 from . import db
 import time
 
-# Select the collection for managing subscriptions
+# Accessing the subscription collection
 db = db.subscription
 
-async def active_sub(user_id: int) -> None:
-    """
-    Activate a subscription by updating or inserting the user ID with the current time.
-    """
+async def active_sub(user_id):
+    """Activate a subscription for the given user_id."""
     await db.update_one({"user_id": user_id}, {"$set": {"time": time.time()}}, upsert=True)
 
-async def get_all_subs() -> dict[int, float]:
-    """
-    Retrieve all subscriptions as a dictionary with user IDs and their subscription times.
-    """
-    cursor = db.find()
-    subs_list = await cursor.to_list(length=None)
-    return {user["user_id"]: user["time"] for user in subs_list}
+async def get_all_subs():
+    """Get all subscriptions."""
+    subscriptions = db.find()
+    subs_list = await subscriptions.to_list(length=None)
+    return {sub["user_id"]: sub["time"] for sub in subs_list}
 
-async def del_sub(user_id: int) -> None:
-    """
-    Delete a subscription by removing the user ID from the subscription collection.
-    """
+async def del_sub(user_id):
+    """Delete the subscription for the given user_id."""
     await db.delete_one({"user_id": user_id})
