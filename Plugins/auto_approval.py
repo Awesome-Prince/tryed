42a37aceb5bd232c4afc1ef10dcc84703f923d00
maniksaluja <69381906+maniksaluja@users.noreply.github.com>
2024-11-28 16:53:33 +0530

    Update auto_approval.py

--- Plugins/auto_approval.py
+++ Plugins/auto_approval.py
@@ -1,15 +1,21 @@
 from pyrogram import Client, filters
-from config import Config
+from config import FSUB
 from Database.settings import get_settings
 
-@Client.on_chat_join_request(filters.chat(Config.FSUB1, Config.FSUB2))
-async def auto_approve(client: Client, request):
-    # Check if auto-approval is enabled in the settings
+@Client.on_chat_join_request(filters.chat(FSUB))
+async def cjr(client: Client, request):
+    """
+    Automatically approve chat join requests if auto-approval is enabled.
+    """
     settings = await get_settings()
-    if not settings.get('auto_approval', False):
+    if not settings['auto_approval']:
         return
 
-    # Approve the join request
-    await client.approve_chat_join_request(request.chat.id, request.from_user.id)
-    # Send a welcome message
+    # Approve the chat join request
+    await client.approve_chat_join_request(
+        request.chat.id,
+        request.from_user.id
+    )
+    
+    # Send a welcome message to the user
     await client.send_message(request.from_user.id, "Hi")
